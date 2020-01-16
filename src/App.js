5f74ebc49770aa23d519665463e4e309e3b3c801
José Matheus <jose.matheus_ext@michelin.com>
2020-01-16 20:08:24 +0000

    Estilização parte 1 - 1:00:03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
+import './Global.css';
 import './App.css';
+import './Sidebar.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div id= "app" >
+     <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GIT</label>
+            <input name="github_username" id="github_username" required></input>
+          </div>
+
+          <div className="input-block">  
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required></input>
+          </div>
+
+          <div className="input-group">         
+
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required></input>
+            </div>
+          
+            <div className="input-block">
+              <label htmlFor="longetude">Longetide</label>
+              <input name="longetude" id="longetude" required></input>
+          </div>
+
+         </div>
+
+         <button type="submit">Salvar</button>
+        </form>
+     </aside>
+     <main>
+       <ul>
+         <li className="dev-item">
+            <header>
+                <img src="" alt=""></img>
+            </header>
+
+         </li>
+       </ul>
+
+     </main>
+
+   </div>
   );
 }
 
